002a3e4cfbd1e4de8690b89b59c8a1f0a2b68812
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-03-20 16:40:27 -0300

    feat(app): add tabela de pessoas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 function App() {
   return (
     <div className="content">
-      <div class="columns is-mobile">
-        <div class="column is-half is-offset-one-quarter">
-        <h1>Relou uordi</h1>
+      <div class="columns is-centered">
+        <div class="column is-half ">
+          <h1>Lista de pessoa</h1>
+          <table class="table ">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>nome</th>
+                <th>cpf</th>
+                <th>nascimento</th>
+                <th>sexo</th>
+                <th>criação</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+               <td>1</td>
+               <td>1</td>
+               <td>1</td>
+               <td>1</td>
+               <td>1</td>
+               <td>1</td>
+                </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div >
